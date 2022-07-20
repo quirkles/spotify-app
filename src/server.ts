17171dde12e7f1e6ee2c24c9b7fe6a94d7f3059e
quirkles <alexquirk@joinsherpa.com>
2022-07-19 20:51:30 -0400

    clean up error handling

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "@koa/cors";
 import { initRoutes } from "./routes";
 import { initializeMiddleware } from "./middleware";
 import Router from "@koa/router";
+import { Logger } from "winston";
 
 export const createServer = (): Koa => {
   const app = new Koa();
@@ -12,21 +13,22 @@ export const createServer = (): Koa => {
 
   initializeMiddleware(app);
 
+  app.use(async function (ctx, next) {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = (err as Error & { status: number }).status || 500;
+      ctx.body = (err as Error).message;
+      ctx.app.emit("error", err, ctx.logger);
+    }
+  });
+
   const router = initRoutes(new Router());
 
   app.use(router.routes());
 
-  app.use(async function (ctx, next) {
-    try {
-      await next;
-    } catch (err) {
-      ctx.status = (err as Error & { status: number }).status || 500;
-      ctx.body = (err as Error).message;
-      ctx.app.emit("error", err);
-    }
-  });
-  app.on("error", (err, ctx) => {
-    ctx.logger.error(err);
+  app.on("error", (err, logger: Logger) => {
+    logger.error(err);
   });
 
   return app;
