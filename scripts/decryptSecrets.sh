0cb438733306db844b7cfb66f2223f952d31056b
quirkles <alexquirk@joinsherpa.com>
2022-07-22 16:04:19 -0400

    add creds folder

--- scripts/decryptSecrets.sh
+++ scripts/decryptSecrets.sh
@@ -1,2 +1,3 @@
-gcloud kms decrypt --key=first-key --keyring=spotify-app-keyring --location=global --plaintext-file=../src/secrets.ts --ciphertext-file=../src/secrets.enc
-gcloud kms decrypt --key=first-key --keyring=spotify-app-keyring --location=global --plaintext-file=../terraform/keys/tf-sa-key.json --ciphertext-file=../terraform/keys/tf-sa-key.enc
+gcloud kms decrypt --key=first-key --keyring=spotify-app-keyring --location=global --plaintext-file=../src/secrets.ts --ciphertext-file=../src/secrets.enc --project=spotify-application-356414
+
+
